cf95b5a4b6d9242765599a8b7d0705066429e53b
alamsyahalamm <muhammad728744@smk.belajar.id>
2024-08-07 19:49:10 +0700

    NOMER-4

--- tugas.js
+++ tugas.js
@@ -8,11 +8,24 @@ console.log(umur);
 const nama = "Faiq";
 console.log(nama); 
 
+
 //2
 const penjumlahan = (a, b) => a + b;
-console.log(penjumlahan(3, 4)); // Output: 7
+console.log(penjumlahan(3, 4)); 
+
 
 //3
 const kota = "Jakarta";
 const ucapan = `Selamat datang di ${kota}!`;
-console.log(ucapan); // Output: Selamat datang di Jakarta!
+console.log(ucapan); 
+
+
+//4
+const buah = ["Apel", "Jeruk", "Mangga"];
+const [buah1, buah2, buah3] = buah;
+console.log(buah1, buah2, buah3);
+
+const mahasiswa = { nama1: "Ali", usia: 22, jurusan: "Teknik Informatika" };
+const { nama1, usia, jurusan } = mahasiswa;
+console.log(nama, usia, jurusan); 
+
