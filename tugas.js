340c7118bc73e84a8f357ee1498e2d3bfd42418e
alamsyahalamm <muhammad728744@smk.belajar.id>
2024-08-07 20:03:59 +0700

    NOMER-7

--- tugas.js
+++ tugas.js
@@ -48,3 +48,26 @@ const  { tambah, kurang } = require ('./hitung.js');
 console.log(tambah(5, 3)); 
 console.log(kurang(5, 3)); 
 
+
+//7
+const getData = () => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve("Data berhasil diambil");
+      }, 2000);
+    });
+  };
+  
+  getData().then(response => console.log(response)); 
+  
+  const fetchData = async () => {
+    try {
+      const response = await getData();
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
+  fetchData(); 
+
