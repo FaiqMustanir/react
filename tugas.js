9eeff24e63a4022bb2b3cdb173ad077aff20bd22
alamsyahalamm <muhammad728744@smk.belajar.id>
2024-08-07 19:46:28 +0700

    NOMER-3

--- tugas.js
+++ tugas.js
@@ -11,3 +11,8 @@ console.log(nama);
 //2
 const penjumlahan = (a, b) => a + b;
 console.log(penjumlahan(3, 4)); // Output: 7
+
+//3
+const kota = "Jakarta";
+const ucapan = `Selamat datang di ${kota}!`;
+console.log(ucapan); // Output: Selamat datang di Jakarta!
